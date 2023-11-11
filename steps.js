6d64af991775ee076ed1086468cb879f5476afb7
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-11-11 09:24:41 +0600

    steps created

---
+++ steps.js
@@ -0,0 +1,12 @@
+// WHEN CREATING THR FIRST REPO
+1. git init
+2. git add.
+3. git commit - m "commit message"
+4. git branch - m main
+5. git remote add origin THE LINK
+6. git push - u origin main
+
+// WHEN UPDATING
+1. git add .
+2. git commit - m "your commit message"
+3. git push
