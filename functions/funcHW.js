17f15f61d25362d01e91cd96f06980deee2a5c2c
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-12-02 00:52:09 +0600

    function class

---
+++ functions/funcHW.js
@@ -0,0 +1,37 @@
+// PROBLEM 1
+
+// function addition(num) {
+//     let sum = 0;
+//     // loop
+//     run a loop from 1 to num.
+//     add all the numbers
+
+//     // conditioning
+//     if sum is greater than or equal to 100, you have to print the sum and print "its a century"
+//     else just print the sum
+
+// }
+// addition(10)
+
+// there's gonna be a function. and in the parameter you're gonna receive the last number of your for loop.You have to run a loop INSIDE the function. The loop is gonna start from 1 to the parameter number.You need to add all the values If the addition of all the numbers is greater than or equal to 100, along side the total value you have to print that "its a century".If the total value is less than 100, just print the total value.
+
+// ------
+
+//PROBLEM 2
+// create a function
+// receive a parameter named num
+// create the multiplication table for num
+
+
+------
+// PROBLEM 3
+// create a function
+// send an array of numbers inside the function
+// calculate the max number of the array
+
+// const arr = [7, 8, 9];
+// function arraycheck(arrrrrr) {
+//     console.log(arrrrrr)
+//     }
+
+// arraycheck(arr)
---
+++ functions/functionsclass1.js
@@ -0,0 +1,64 @@
+// 3 types
+// Traditional Function
+// Arrow Function
+// Anonymous Function
+
+// EcmaScript 6
+// Es-6 of js
+
+// const cocopowder = true;
+
+// function bakeCookies() {
+//     console.log("This helps me to bake");
+// }
+
+// if (cocopowder === true) {
+//     bakeCookies();
+// } else {
+//     console.log("I can't bake cookies");
+// }
+
+// you have to be a women
+// you have to be 25yo
+// .....
+
+// recieving parameters
+// function eligible(gender, age) {
+//     if(gender === "women" && age >= 25) {
+//         return true;
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+// const res = eligible("women", 24);
+// console.log(res)
+// function hire(res) {
+//     if (res === true) {
+//         console.log("hire this person")
+//     }
+    
+// }
+
+// // sending arguments
+// addOurAges(70, 80, 90);
+
+// suppose youre gonna fly somewhere.there are 3 peoples flying.the parents and the child.If the child is older than 5 years, we only need to add the age of the parents.But if the kid is older than 5, we have to add all the ages and return the total age.
+
+// function
+// receive 3 parameters
+// send 3 arguements
+// in the function check the childs age
+// if more than 5, add everyones age
+// if not, add the parents age only
+
+
+function addition(a, b, c) {
+    const res = a + b + c;
+    console.log(res)
+}
+ addition(1, 2, 3);
+// console.log(add)
+
+// sum of numbers
